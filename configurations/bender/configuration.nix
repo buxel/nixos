31b236e410acfe1b6fd60377dd0a011ddc8cc53c
buxel <buxel.dev@gmail.com>
2023-11-11 15:44:43 +0100

    more logging

--- configurations/bender/configuration.nix
+++ configurations/bender/configuration.nix
@@ -35,6 +35,7 @@
   #modules.blobfuse.mounts."${config.modules.ocis.dataDir}" = {
     configPath = config.age.secrets."blobfuse.yaml".path;
     container = "ocis";
+    mountOpts = [ "--log-level=LOG_DEBUG" ];
     # uid = config.ids.uids.ocis;
     # gid = config.ids.gids.ocis;
   }; 
