b5fe9f7fc08e7e61e87f8baef6ca517519eb7743
buxel <buxel.dev@gmail.com>
2023-11-11 19:03:48 +0100

    up

--- modules/blobfuse/default.nix
+++ modules/blobfuse/default.nix
@@ -73,7 +73,7 @@ let
           # blobfuse  
           "--config-file=${values.configPath}"
           "--container-name=${values.container}"
-          "--foreground"
+          #"--foreground"
           # "--allow-other"
           # "--tmp-path=/tmp/"
           
