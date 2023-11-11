499d46288b290de44c6749dcdc7987688e5f7410
buxel <buxel.dev@gmail.com>
2023-11-11 19:52:48 +0100

    path workaround

--- configurations/bender/configuration.nix
+++ configurations/bender/configuration.nix
@@ -31,6 +31,7 @@
   # Ocis remote user data 
   # NOTE: rclone does not support symlinks, which OCIS uses.
   #modules.blobfuse.mounts."/mnt/ocis" = {
+  age.secrets."blobfuse-yaml".path = config.age.secretsDir + "/blobfuse.yaml"; # blobfuse inists on a ".yaml extension. This messes up secrets handling with "nixos secrets", as dots indicate a hierarchy level in nix.
   modules.blobfuse.package = pkgs.unstable.blobfuse;
   modules.blobfuse.mounts."${config.modules.ocis.dataDir}" = {
     configPath = config.age.secrets."blobfuse-yaml".path;
