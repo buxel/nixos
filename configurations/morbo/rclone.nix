369eea10ce85eac5595032ebe1aaa3b2448ad138
Buxel <buxel.dev@gmail.com>
2024-05-24 10:06:26 -0600

    fix rclone

--- configurations/morbo/rclone.nix
+++ configurations/morbo/rclone.nix
@@ -1,10 +1,28 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 let
   inherit (config.age) secrets;
+  target = "/mnt/rclone/spica/";
+  #target = "/mnt/spica/";
 in
 {
   environment.systemPackages = with pkgs; [ rclone ];
   
+  users = {
+    users = {
+
+      # Create user
+      rclone = {
+        isSystemUser = true;
+        group = "media";
+        description = "rclone daemon user";
+        home = "/mnt/rclone";
+        homeMode = "770";
+        createHome = true;
+        extraGroups = [ "secrets" ]; 
+      };
+    };
+  };
+
   # RClone service
   systemd.services.rclone-mount = {
     # Ensure the service starts after the network is up
@@ -15,26 +33,31 @@ in
     # Service configuration
     serviceConfig = {
       Type = "simple";
-      ExecStartPre = "/run/current-system/sw/bin/mkdir -p /mnt/rclone/seedbox/"; # Creates folder if didn't exist
-      ExecStart = "${pkgs.rclone}/bin/rclone mount --config ${secrets.rclone-conf.path} spica: /mnt/spica"; # Mounts
-      #ExecStart = "${pkgs.rclone}/bin/rclone mount --config /home/me/.config/rclone/rclone.conf spica: /mnt/rclone/seedbox/"; # Mounts
-      ExecStop = "/run/current-system/sw/bin/fusermount -u /mnt/rclone/seedbox/"; # Dismounts
-      Restart = "on-failure";
-      RestartSec = "10s";
-      # PermissionsStartOnly = true; # Use root to create the mountpoint, run with user:group specified below
-      User = "root";
+      ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${target}"; # Creates folder if didn't exist
+      ExecStart =  ''
+        ${pkgs.rclone}/bin/rclone mount spica: ${target} \
+          --config ${secrets.rclone-conf.path} \
+          --no-modtime \
+          --allow-other \
+          --dir-perms 0775 \
+          --file-perms 0775 
+        '';
+      ExecStop = "/run/current-system/sw/bin/fusermount -u ${target}"; # Dismounts
+      User = "rclone";
       Group = "media";
+      Restart = "always";
+      RestartSec = "10s";
       Environment = [ "PATH=/run/wrappers/bin/:$PATH" ]; # Required environments
     };
   };
 
-  systemd.mounts = [{
-    after = [ "network-online.target" ];
-    type = "rclone";
-    what = "spica:";
-    where = "/mnt/spica2";
-    # rw,_netdev,allow_other,args2env,vfs-cache-mode=writes
-    options = "rw,_netdev,allow_other,args2env,vfs-cache-mode=writes,config=${secrets.rclone-conf.path},cache-dir=/var/rclone,verbose=2";
-  }];
+  # systemd.mounts = [{
+  #   after = [ "network-online.target" ];
+  #   type = "rclone";
+  #   what = "spica:";
+  #   where = "/mnt/spica2";
+  #   # rw,_netdev,allow_other,args2env,vfs-cache-mode=writes
+  #   options = "rw,_netdev,allow_other,args2env,vfs-cache-mode=writes,config=${secrets.rclone-conf.path},cache-dir=/var/rclone,verbose=2";
+  # }];
 }
 
